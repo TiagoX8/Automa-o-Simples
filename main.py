fe6942957ec44c09f3ae161d68dcb7af24ca5433
Tiago <thiagokroos21@gmail.com>
2023-01-16 11:45:46 -0300

    Auto

---
+++ main.py
@@ -0,0 +1,23 @@
+import pyautogui
+import time
+
+ #while True:
+  # pos = input('Pegar pos? (s/n)').lower()
+   # if pos == 's':
+    #    print(pyautogui.position())
+    #else:
+     #   break
+
+pyautogui.moveTo(562,762,2)
+pyautogui.click()
+pyautogui.moveTo(481,420,2)
+pyautogui.write('youtube')
+pyautogui.hotkey('enter')
+pyautogui.moveTo(225,296,2)
+pyautogui.click()
+pyautogui.moveTo(435,97,2)
+pyautogui.click()
+pyautogui.write('Enemy')
+pyautogui.hotkey('enter')
+pyautogui.moveTo(425,473,2)
+pyautogui.click()
